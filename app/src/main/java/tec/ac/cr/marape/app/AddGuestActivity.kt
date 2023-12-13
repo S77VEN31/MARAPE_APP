1e0e7e8a7f1a8b62a6dddcff470b9692fd09391b
Mariana <mfernandezmar3@gmail.com>
2023-12-13 08:28:07 -0600

    GuestsList and AddGuest Activity

---
+++ app/src/main/java/tec/ac/cr/marape/app/AddGuestActivity.kt
@@ -0,0 +1,11 @@
+package tec.ac.cr.marape.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddGuestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_guest)
+    }
+}
