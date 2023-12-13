bc3c579d04f9ae10ab66f3ac769746da98e0fd33
Mariana <mfernandezmar3@gmail.com>
2023-12-13 08:32:17 -0600

    EditProduct Activity

---
+++ app/src/main/java/tec/ac/cr/marape/app/EditProductActivity.kt
@@ -0,0 +1,11 @@
+package tec.ac.cr.marape.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_product)
+    }
+}
