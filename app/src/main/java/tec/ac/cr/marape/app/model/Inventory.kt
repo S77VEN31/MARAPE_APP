0780da7a10619c3ea4f5f4f7526780f85bac45da
zSnails <erizojuan33@gmail.com>
2023-12-14 11:28:22 -0600

    chore(format): Run formatter on kotlin source files.

--- app/src/main/java/tec/ac/cr/marape/app/model/Inventory.kt
+++ app/src/main/java/tec/ac/cr/marape/app/model/Inventory.kt
@@ -3,17 +3,17 @@ package tec.ac.cr.marape.app.model
 import java.time.LocalDate
 
 enum class InventoryStatus(var status: Int) {
-    INACTIVE(0),
-    ACTIVE(1);
-    companion object {
-        fun fromInt(value: Int) = InventoryStatus.values().first { it.status == value }
-    }
+  INACTIVE(0),
+  ACTIVE(1);
+  companion object {
+    fun fromInt(value: Int) = InventoryStatus.values().first { it.status == value }
+  }
 }
 
 data class Inventory(
-        var name: String,
-        var creationDate: LocalDate,
-        val status: InventoryStatus,
-        var adminUserId: String,
-        val invitedUsers: List<String>
+    var name: String,
+    var creationDate: LocalDate,
+    val status: InventoryStatus,
+    var adminUserId: String,
+    val invitedUsers: List<String>
 )
--- app/src/main/java/tec/ac/cr/marape/app/model/User.kt
+++ app/src/main/java/tec/ac/cr/marape/app/model/User.kt
@@ -1,9 +1,8 @@
 package tec.ac.cr.marape.app.model
 
 data class User(
-        var nombre: String,
-        var email: String,
-        var profile: String,
-        var isAdmin: Boolean,
-        var activeSession: Boolean
+    var name: String,
+    var email: String,
+    var profile: String,
+    var isAdmin: Boolean,
 )
