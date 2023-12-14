47674aa6964ecbc84ae13612fd141ec354a86110
zSnails <erizojuan33@gmail.com>
2023-12-14 05:33:58 -0600

    style: Run formatter.

--- app/src/main/java/tec/ac/cr/marape/app/MainActivity.kt
+++ app/src/main/java/tec/ac/cr/marape/app/MainActivity.kt
@@ -12,31 +12,27 @@ import tec.ac.cr.marape.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var db: FirebaseFirestore
+  private lateinit var binding: ActivityMainBinding
+  private lateinit var db: FirebaseFirestore
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        db = FirebaseFirestore.getInstance()
+    db = FirebaseFirestore.getInstance()
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+    val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration =
-                AppBarConfiguration(
-                        setOf(
-                                R.id.navigation_home,
-                                R.id.navigation_dashboard,
-                                R.id.navigation_notifications
-                        )
-                )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
+    val navController = findNavController(R.id.nav_host_fragment_activity_main)
+    // Passing each menu ID as a set of Ids because each
+    // menu should be considered as top level destinations.
+    val appBarConfiguration =
+        AppBarConfiguration(
+            setOf(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+        )
+    setupActionBarWithNavController(navController, appBarConfiguration)
+    navView.setupWithNavController(navController)
+  }
 }
