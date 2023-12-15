7462c86f1b509d6030feaf0c3295359e7ede1857
zSnails <erizojuan33@gmail.com>
2023-12-15 13:00:35 -0600

    feat: Now inventories will be loaded upon launching the application.
    Once the application launches all owned inventories will be loaded,
    however this could come at a memory cost, since we can have 'unlimited'
    inventories, we'd be better off lazy loading inventories on batches

--- app/src/main/java/tec/ac/cr/marape/app/Marape.kt
+++ app/src/main/java/tec/ac/cr/marape/app/Marape.kt
@@ -1,14 +1,19 @@
 package tec.ac.cr.marape.app
 
 import android.app.Application
+import com.google.firebase.firestore.Filter
+import com.google.firebase.firestore.FirebaseFirestore
+import tec.ac.cr.marape.app.model.Inventory
 import tec.ac.cr.marape.app.model.User
 import tec.ac.cr.marape.app.state.State
 
 class Marape : Application() {
   private lateinit var state: State
-  public override fun onCreate() {
+  private lateinit var db: FirebaseFirestore
+  override fun onCreate() {
     super.onCreate()
     state = State.getInstance(baseContext)
+    db = FirebaseFirestore.getInstance()
 
     state.user = User(
       "Aaron González",
@@ -17,5 +22,18 @@ class Marape : Application() {
       "Costa Rica",
       "6475-0398"
     )
+
+    state.inventories = ArrayList()
+
+    val inventoriesRef =
+      db.collection("inventories").where(Filter.equalTo("ownerEmail", state.user.email))
+    inventoriesRef.get().addOnSuccessListener { snapshot ->
+      snapshot.documents.iterator().forEach { inventorySnapshot ->
+        val inventory = inventorySnapshot.toObject(Inventory::class.java)
+        inventory?.id = inventorySnapshot.id
+        inventory?.let { state.inventories.add(it) }
+      }
+    }
+
   }
 }
