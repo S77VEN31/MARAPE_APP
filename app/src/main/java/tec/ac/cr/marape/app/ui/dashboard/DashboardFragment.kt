11b9a6a633ba25a512d273ffc8aead6cd89596c8
zSnails <erizojuan33@gmail.com>
2023-12-15 17:49:59 -0600

    feat: Now creating a new inventory will reload the recycler view.
    The way this has been done is pretty hacky tbh, if anyone wants to try
    and fix it a shot then be my guest.

--- app/src/main/java/tec/ac/cr/marape/app/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/tec/ac/cr/marape/app/ui/dashboard/DashboardFragment.kt
@@ -1,10 +1,15 @@
 package tec.ac.cr.marape.app.ui.dashboard
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.activity.result.registerForActivityResult
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,41 +18,50 @@ import tec.ac.cr.marape.app.CreateInventoryActivity
 import tec.ac.cr.marape.app.adapter.InventoryView
 import tec.ac.cr.marape.app.databinding.FragmentDashboardBinding
 import tec.ac.cr.marape.app.state.State
+import tec.ac.cr.marape.app.R
 
 class DashboardFragment : Fragment() {
 
   private var _binding: FragmentDashboardBinding? = null
-  private lateinit var recyclerView: RecyclerView
+  private var recyclerView: RecyclerView? = null
   private lateinit var state: State
+  private lateinit var launcher: ActivityResultLauncher<Intent>
 
-  // This property is only valid between onCreateView and
-  // onDestroyView.
   private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val dashboardViewModel =
-      ViewModelProvider(this).get(DashboardViewModel::class.java)
+  @SuppressLint("NotifyDataSetChanged")
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-    state = State.getInstance(null)
-    _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-    val root: View = binding.root
+    recyclerView = binding.ownedInventoriesRecycler
+    recyclerView?.setHasFixedSize(false)
+    recyclerView?.adapter = InventoryView(state.inventories)
+    recyclerView?.layoutManager = LinearLayoutManager(activity)
+
+    launcher = registerForActivityResult(StartActivityForResult()) {
+      // Receive update from the launched intent
+      recyclerView?.adapter?.notifyDataSetChanged()
+    }
+
     binding.floatingActionButton.setOnClickListener { _ ->
       val intent = Intent(activity, CreateInventoryActivity::class.java)
-      startActivity(intent)
+      launcher.launch(intent)
     }
-    recyclerView = binding.ownedInventoriesRecycler
-    recyclerView.layoutManager = LinearLayoutManager(context)
-    recyclerView.setHasFixedSize(false)
-    recyclerView.adapter = InventoryView(state.inventories)
-    return root
+
+  }
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+  ): View {
+    val dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+    state = State.getInstance(null)
+    _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+    return binding.root
   }
 
   override fun onDestroyView() {
     super.onDestroyView()
     _binding = null
+    recyclerView = null
   }
 }
