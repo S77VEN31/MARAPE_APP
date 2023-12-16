3592412d199461a769b5e41e27753e1b5273f46d
zSnails <erizojuan33@gmail.com>
2023-12-16 08:34:35 -0600

    fix: Add account deletion confirmation dialog.
    For some reason there wasn't a confirmation dialog, and we really can't afford to not have one, a simple click
    on the button would have deleted that user's account, although inventories aren't automatically deleted when
    the account is deleted so that would've been fine

--- app/src/main/java/tec/ac/cr/marape/app/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/tec/ac/cr/marape/app/ui/notifications/NotificationsFragment.kt
@@ -1,5 +1,6 @@
 package tec.ac.cr.marape.app.ui.notifications
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,9 +9,10 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import tec.ac.cr.marape.app.LoginActivity
+import tec.ac.cr.marape.app.R
 import tec.ac.cr.marape.app.databinding.FragmentNotificationsBinding
+import tec.ac.cr.marape.app.state.State
 
 
 class NotificationsFragment : Fragment() {
@@ -19,6 +21,8 @@ class NotificationsFragment : Fragment() {
   private val binding get() = _binding!!
 
   private lateinit var mAuth: FirebaseAuth
+
+  private lateinit var state: State
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -29,12 +33,23 @@ class NotificationsFragment : Fragment() {
     val root: View = binding.root
 
     mAuth = FirebaseAuth.getInstance()
-    val user: FirebaseUser? = mAuth.currentUser
 
-    // Accede a tus vistas a través de binding
-    user?.email?.let {
-      binding.etEmail.text = it
+    // This should never fail
+    context?.let {
+      state = State.getInstance(it)
     }
+
+    return root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    binding.etEmail.text = state.user.email
+    binding.etUser.text = state.user.name
+    binding.etCountry.text = state.user.country
+    binding.etPhone.text = state.user.phone
+
 
     // Botón para cerrar sesión
     binding.btnSignOff.setOnClickListener {
@@ -48,23 +63,26 @@ class NotificationsFragment : Fragment() {
     binding.deleteAccount.setOnClickListener {
       deleteAccount()
     }
-
-
-    return root
   }
 
   private fun deleteAccount() {
-    val user = mAuth.currentUser
-
-    user?.delete()?.addOnSuccessListener {
-      startActivity(Intent(requireContext(), LoginActivity::class.java))
-    }?.addOnFailureListener {
-      Toast.makeText(
-        requireContext(),
-        "Error al eliminar la cuenta: ${it.message}",
-        Toast.LENGTH_SHORT
-      ).show()
+    val builder = AlertDialog.Builder(context)
+    builder.setCancelable(true)
+    builder.setMessage(R.string.account_deletion_confirmation_message)
+    builder.setTitle(R.string.account_deletion_title)
+    builder.setPositiveButton(R.string.account_deletion_confirm_button_text) { _, _ ->
+      mAuth.currentUser?.delete()?.addOnSuccessListener {
+        startActivity(Intent(requireContext(), LoginActivity::class.java))
+      }?.addOnFailureListener {
+        Toast.makeText(
+          requireContext(),
+          it.message, // Here I removed the rest of the message because it wouldn't show, and it's more important
+          // to let the user know what's wrong rather than to shadow it by an useless message.
+          Toast.LENGTH_SHORT
+        ).show()
+      }
     }
+    builder.create().show()
   }
 
   override fun onDestroyView() {
