b9bc86399d8513389a806568d026ff177a6cf837
zSnails <erizojuan33@gmail.com>
2023-12-16 18:41:58 -0600

    fix: Change marape class' responsibilities.
    Now the marape class will not be responsible for loading the user's
    inventories, the only reason this was working was because the user was
    hard coded into the code, I realized as soon as I launched the
    application, now that won't happen again.

--- app/src/main/java/tec/ac/cr/marape/app/Marape.kt
+++ app/src/main/java/tec/ac/cr/marape/app/Marape.kt
@@ -2,7 +2,6 @@ package tec.ac.cr.marape.app
 
 import android.app.Application
 import com.google.firebase.firestore.FirebaseFirestore
-import tec.ac.cr.marape.app.model.User
 import tec.ac.cr.marape.app.state.State
 
 class Marape : Application() {
@@ -12,15 +11,6 @@ class Marape : Application() {
     super.onCreate()
     state = State.getInstance(baseContext)
     db = FirebaseFirestore.getInstance()
-
-    // FIXME: Remove this, the only reason this is here is because proper authentication has not been properly setup yet
-    state.user = User(
-      "Aaron González",
-      "erizojuan33@gmail.com",
-      "Something here I guess lol",
-      "Costa Rica",
-      "6475-0398"
-    )
 
     state.inventories = ArrayList()
 
