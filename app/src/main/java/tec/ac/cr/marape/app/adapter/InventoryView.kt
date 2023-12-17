be2e6005ede7272c82535be6d5fa8ed2ff480eda
zSnails <erizojuan33@gmail.com>
2023-12-16 18:27:09 -0600

    feat(WIP): Implement fuzzy searching for the search bar on the owned inventories view.

--- app/src/main/java/tec/ac/cr/marape/app/adapter/InventoryView.kt
+++ app/src/main/java/tec/ac/cr/marape/app/adapter/InventoryView.kt
@@ -4,27 +4,32 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageButton
 import android.widget.Switch
 import android.widget.TextView
-import androidx.appcompat.widget.SwitchCompat
 import androidx.recyclerview.widget.RecyclerView
+import me.xdrop.fuzzywuzzy.FuzzySearch
 import tec.ac.cr.marape.app.R
 import tec.ac.cr.marape.app.model.Inventory
 import java.text.DateFormat
-import java.time.LocalDate
 import java.util.Date
 import java.util.Locale
 
+class InventoryView(var inventories: ArrayList<Inventory>) :
+  RecyclerView.Adapter<InventoryView.ViewHolder>(), Filterable {
 
-class InventoryView(private var inventories: ArrayList<Inventory>) :
-  RecyclerView.Adapter<InventoryView.ViewHolder>() {
+
+  var inventoriesFull = inventories.toList()
 
   private val locale = Locale("es", "CR")
   private val formatter = DateFormat.getDateInstance(DateFormat.DEFAULT, locale)
+  private lateinit var deleteHandler: (Inventory, Int) -> Unit
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-    val itemView = LayoutInflater.from(parent.context).inflate(R.layout.layout_inventory_entry, parent, false)
+    val itemView =
+      LayoutInflater.from(parent.context).inflate(R.layout.layout_inventory_entry, parent, false)
     return ViewHolder(itemView)
   }
 
@@ -37,12 +42,20 @@ class InventoryView(private var inventories: ArrayList<Inventory>) :
     holder.inventoryName.text = currentInventory.name
     holder.creationDate.text = formatter.format(Date(currentInventory.creationDate)).toString()
     holder.statusSwitch.isChecked = currentInventory.status.status == 1
-    holder.collaborators.text = currentInventory.invitedUsers?.size.toString()
+    holder.collaborators.text = currentInventory.invitedUsers.size.toString()
+    holder.deleteInventoryButton.setOnClickListener {
+      deleteHandler(currentInventory, position)
+    }
+  }
+
+  fun setDeleteHandler(handler: (Inventory, Int) -> Unit) {
+    deleteHandler = handler
   }
 
   class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val inventoryName: TextView = itemView.findViewById(R.id.entry_inventory_name)
     val creationDate: TextView = itemView.findViewById(R.id.entry_inventory_creation_date)
+
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     val statusSwitch: Switch = itemView.findViewById(R.id.entry_inventory_status_switch)
     val collaborators: TextView = itemView.findViewById(R.id.entry_inventory_collaborators)
@@ -51,4 +64,40 @@ class InventoryView(private var inventories: ArrayList<Inventory>) :
     val deleteInventoryButton: ImageButton =
       itemView.findViewById(R.id.entry_delete_inventory_button)
   }
+
+  private val itemsFilter: Filter = object : Filter() {
+    override fun performFiltering(constraint: CharSequence?): FilterResults {
+      val filteredList = ArrayList<Inventory>()
+
+      if (constraint == null || constraint.length == 0) {
+        filteredList.addAll(inventoriesFull)
+      } else {
+        val query = constraint.toString().lowercase().trim()
+        for (item in inventoriesFull) {
+          if (FuzzySearch.ratio(query, item.name) > 30) {
+            filteredList.add(item)
+          }
+        }
+      }
+
+      val results = FilterResults()
+      results.values = filteredList
+      return results
+    }
+
+    private var first = true
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+      inventories.clear()
+      inventories.addAll(results?.values as List<Inventory>)
+      notifyDataSetChanged()
+    }
+
+  }
+
+  override fun getFilter(): Filter {
+    return itemsFilter
+  }
+
 }
--- app/src/main/java/tec/ac/cr/marape/app/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/tec/ac/cr/marape/app/ui/dashboard/DashboardFragment.kt
@@ -2,23 +2,35 @@ package tec.ac.cr.marape.app.ui.dashboard
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
-import androidx.activity.result.registerForActivityResult
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 import tec.ac.cr.marape.app.CreateInventoryActivity
+import tec.ac.cr.marape.app.R
 import tec.ac.cr.marape.app.adapter.InventoryView
 import tec.ac.cr.marape.app.databinding.FragmentDashboardBinding
+import tec.ac.cr.marape.app.model.Inventory
 import tec.ac.cr.marape.app.state.State
-import tec.ac.cr.marape.app.R
 
 class DashboardFragment : Fragment() {
 
@@ -26,36 +38,101 @@ class DashboardFragment : Fragment() {
   private var recyclerView: RecyclerView? = null
   private lateinit var state: State
   private lateinit var launcher: ActivityResultLauncher<Intent>
+  private lateinit var viewModel: Lazy<DashboardViewModel>
+  private lateinit var db: FirebaseFirestore
+
 
   private val binding get() = _binding!!
 
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
   @SuppressLint("NotifyDataSetChanged")
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
     recyclerView = binding.ownedInventoriesRecycler
-    recyclerView?.setHasFixedSize(false)
-    recyclerView?.adapter = InventoryView(state.inventories)
-    recyclerView?.layoutManager = LinearLayoutManager(activity)
+    recyclerView!!.setHasFixedSize(false)
+    val customAdapter = InventoryView(viewModel.value.inventories)
+    customAdapter.setDeleteHandler(::handleInventoryDeletion)
+    recyclerView!!.adapter = customAdapter
+    recyclerView!!.layoutManager = LinearLayoutManager(activity)
 
-    launcher = registerForActivityResult(StartActivityForResult()) {
-      // Receive update from the launched intent
-      recyclerView?.adapter?.notifyDataSetChanged()
+    launcher = registerForActivityResult(StartActivityForResult(), ::resultCallback)
+
+    binding.floatingActionButton.setOnClickListener(::createInventory)
+
+    (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+      override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.search_inventory, menu)
+        val searchItem: MenuItem = menu.findItem(R.id.search_inventory)
+        (searchItem.actionView as SearchView).setOnQueryTextListener(object :
+          SearchView.OnQueryTextListener {
+          override fun onQueryTextSubmit(query: String?): Boolean {
+            return true
+          }
+
+          override fun onQueryTextChange(newText: String?): Boolean {
+            customAdapter.filter.filter(newText)
+            return true
+          }
+
+        })
+      }
+
+      override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return false
+      }
+
+    }, viewLifecycleOwner)
+
+    recyclerView!!.adapter?.notifyDataSetChanged()
+  }
+
+  private fun handleInventoryDeletion(inventory: Inventory, position: Int) {
+    AlertDialog.Builder(requireContext())
+      .setTitle(R.string.inventory_deletion_title)
+      .setMessage(R.string.inventory_deletion_message)
+      .setCancelable(true)
+      .setPositiveButton(R.string.account_deletion_confirm_button_text) { _, _ ->
+        db.collection("inventories").document(inventory.id).delete().addOnSuccessListener {
+          viewModel.value.remove(inventory)
+          recyclerView!!.adapter?.notifyItemRemoved(position)
+        }
+      }
+      .setNegativeButton(R.string.action_cancel) { self, _ ->
+        self.cancel()
+      }
+      .show()
+  }
+
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+  private fun resultCallback(result: ActivityResult) {
+    val createdInventory = result.data?.getSerializableExtra("created", Inventory::class.java)
+    createdInventory?.let { inventory ->
+      viewModel.value.add(inventory)
+      recyclerView!!.adapter?.notifyItemInserted(0)
     }
+  }
 
-    binding.floatingActionButton.setOnClickListener { _ ->
-      val intent = Intent(activity, CreateInventoryActivity::class.java)
-      launcher.launch(intent)
+  private fun createInventory(view: View) {
+    val intent = Intent(activity, CreateInventoryActivity::class.java)
+    launcher.launch(intent)
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  class ViewModelProducer(val state: State) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+      return DashboardViewModel(state.inventories) as T
     }
-
   }
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
   ): View {
-    val dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+    //val dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
     state = State.getInstance(null)
     _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+    viewModel = viewModels<DashboardViewModel>(factoryProducer = { ViewModelProducer(state) })
+    db = FirebaseFirestore.getInstance()
     return binding.root
   }
 
