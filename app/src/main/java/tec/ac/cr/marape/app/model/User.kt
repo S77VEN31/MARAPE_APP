6b5e97503f4dccffd81c1083b671c52e9a353363
zSnails <erizojuan33@gmail.com>
2023-12-27 15:18:47 -0600

    fix: Add extra reference field to store a reference to self.

--- app/src/main/java/tec/ac/cr/marape/app/model/User.kt
+++ app/src/main/java/tec/ac/cr/marape/app/model/User.kt
@@ -1,9 +1,12 @@
 package tec.ac.cr.marape.app.model
 
+import com.google.firebase.firestore.DocumentReference
+
 data class User(
   var name: String = "",
   var email: String = "",
   var profile: String = "",
   var country: String = "",
-  var phone: String = ""
+  var phone: String = "",
+  var ref: DocumentReference? = null
 )
