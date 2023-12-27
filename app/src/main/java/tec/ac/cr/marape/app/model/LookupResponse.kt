e7f39a878279fb5ec737d8f74f43097420810641
zSnails <erizojuan33@gmail.com>
2023-12-27 11:07:26 -0600

    fix: Add missing fields for the `LookupResponse` class.
    BREAKING_CHANGE: field `barcode_number` has been replaced with `barcode`.

--- app/src/main/java/tec/ac/cr/marape/app/model/LookupResponse.kt
+++ app/src/main/java/tec/ac/cr/marape/app/model/LookupResponse.kt
@@ -1,10 +1,18 @@
 package tec.ac.cr.marape.app.model
 
+import com.google.gson.annotations.SerializedName
+
 data class LookupResponse(var products: List<Product>) {
   data class Product(
-    var barcode_number: String,
+    @SerializedName("barcode_number")
+    var barcode: String,
     var title: String,
     var stores: List<Store>,
+    var brand: String,
+    var description: String,
+    var color: String,
+    var material: String,
+    var size: String,
   ) {
     data class Store(
       var name: String,
