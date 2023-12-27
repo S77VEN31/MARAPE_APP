42e59f2e5959fd81f053ce54b1d218a1fdc0f5ec
zSnails <erizojuan33@gmail.com>
2023-12-27 11:05:32 -0600

    feart(WIP): Implement product creation.
    The product creation feature is still pretty lackluster, although if you
    manually input a bar code it'll fill in some of the product's data for
    you.

--- app/src/main/java/tec/ac/cr/marape/app/CreateProductActivity.kt
+++ app/src/main/java/tec/ac/cr/marape/app/CreateProductActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doAfterTextChanged
+import com.google.firebase.firestore.FirebaseFirestore
 import tec.ac.cr.marape.app.model.Product
 import tec.ac.cr.marape.app.networking.RemoteApi
 import java.util.Timer
@@ -17,21 +18,29 @@ class CreateProductActivity : AppCompatActivity() {
 
   private val product = Product()
   private var timer = Timer()
+  private lateinit var db: FirebaseFirestore
   private lateinit var nameEntry: TextView
+  private lateinit var priceEntry: TextView
+  private lateinit var brandEntry: TextView
+  private lateinit var descriptionEntry: TextView
+  private lateinit var colorEntry: TextView
+  private lateinit var materialEntry: TextView
+  private lateinit var sizeEntry: TextView
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_create_product)
 
+    db = FirebaseFirestore.getInstance()
     nameEntry = findViewById<TextView>(R.id.create_product_name)
     nameEntry.addTextChangedListener {
       product.name = it.toString()
     }
-    findViewById<TextView>(R.id.create_product_brand).addTextChangedListener {
+    brandEntry = findViewById<TextView>(R.id.create_product_brand)
+    brandEntry.addTextChangedListener {
       product.brand = it.toString()
     }
     val t = this
     findViewById<TextView>(R.id.create_product_barcode).doAfterTextChanged {
-      Log.d("Z:Fetch", "Before stopping timer")
       timer.cancel()
       timer = Timer()
       timer.schedule(500L) {
@@ -42,20 +51,25 @@ class CreateProductActivity : AppCompatActivity() {
     findViewById<TextView>(R.id.create_product_amount).addTextChangedListener {
       product.amount = it.toString().toInt()
     }
-    findViewById<TextView>(R.id.create_product_price).addTextChangedListener {
-      product.price = it.toString().toFloat()
+    priceEntry = findViewById<TextView>(R.id.create_product_price)
+    priceEntry.addTextChangedListener {
+      product.targetPrice = it.toString().toFloat()
     }
-    findViewById<TextView>(R.id.create_product_description).addTextChangedListener {
+    descriptionEntry = findViewById<TextView>(R.id.create_product_description)
+    descriptionEntry.addTextChangedListener {
       product.description = it.toString()
     }
-    findViewById<TextView>(R.id.create_product_color).addTextChangedListener {
+    colorEntry = findViewById<TextView>(R.id.create_product_color)
+    colorEntry.addTextChangedListener {
       // TODO: Change the color input for something else.
       product.color = it.toString()
     }
-    findViewById<TextView>(R.id.create_product_material).addTextChangedListener {
+    materialEntry = findViewById<TextView>(R.id.create_product_material)
+    materialEntry.addTextChangedListener {
       product.material = it.toString()
     }
-    findViewById<TextView>(R.id.create_product_size).addTextChangedListener {
+    sizeEntry = findViewById<TextView>(R.id.create_product_size)
+    sizeEntry.addTextChangedListener {
       product.size = it.toString()
     }
     findViewById<TextView>(R.id.create_product_our_price).addTextChangedListener {
@@ -64,23 +78,55 @@ class CreateProductActivity : AppCompatActivity() {
   }
 
   private fun fetchTargetPrice(code: String) {
-    RemoteApi.getProduct(code) { res ->
-      Log.d("Z:Fetch", "Fetching product $code")
+    RemoteApi.getProduct(code, { res ->
       if (res.products.isNotEmpty()) {
-        Log.d("Z:Fetch", "Wasn't empty")
-        val product = res.products[0]
-        val target = product.stores.find {
-          it.name.contains("target", true)
+        val prod = res.products[0]
+        val target = prod.stores.find {
+          it.name.contains(
+            "target",
+            true
+          )
         }
+
+        nameEntry.text = prod.title
+        brandEntry.text = prod.brand
+        descriptionEntry.text = prod.description
+        colorEntry.text = prod.color
+        materialEntry.text = prod.material
+        sizeEntry.text = prod.size
+
         target?.let {
-          Log.d("Z:Fetch", "Something here")
-          nameEntry.text = product.title
+          priceEntry.text = it.price
         }
       }
-    }
+    }, {
+      runOnUiThread {
+        Toast.makeText(
+          this@CreateProductActivity,
+          R.string.barcode_not_found_error,
+          Toast.LENGTH_LONG
+        ).show()
+      }
+    })
   }
 
   fun createProduct(view: View) {
-
+    val products = db.collection("products")
+    val doc = when (product.barcode) {
+      "" -> products.document()
+      else -> products.document(product.barcode)
+    }
+    product.id = doc.id
+    doc.set(product).addOnFailureListener {
+      Log.e("Z:Fetch", it.message.toString())
+    }
+      .addOnSuccessListener {
+        Toast.makeText(
+          this@CreateProductActivity,
+          R.string.create_product_success,
+          Toast.LENGTH_LONG
+        ).show()
+        finish()
+      }
   }
 }
