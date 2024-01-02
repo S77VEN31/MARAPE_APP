f79cf9982279d8b097d21932aa11d6a1f5ed48d3
zSnails <erizojuan33@gmail.com>
2024-01-01 19:16:09 -0600

    feat: Now the inventory details will update if the inventory gets edited.

--- app/src/main/java/tec/ac/cr/marape/app/InventoryDetailsActivity.kt
+++ app/src/main/java/tec/ac/cr/marape/app/InventoryDetailsActivity.kt
@@ -8,6 +8,9 @@ import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
@@ -15,6 +18,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 import tec.ac.cr.marape.app.databinding.ActivityInventoryDetailsBinding
 import tec.ac.cr.marape.app.model.Inventory
 import tec.ac.cr.marape.app.model.User
+import tec.ac.cr.marape.app.state.State
+import tec.ac.cr.marape.app.ui.dashboard.EDITED_INVENTORY
 import java.text.DateFormat
 import java.util.Date
 import java.util.Locale
@@ -22,15 +27,21 @@ import java.util.Locale
 class InventoryDetailsActivity : AppCompatActivity() {
   private lateinit var db: FirebaseFirestore
   private lateinit var owner: User
-  private lateinit var inventory: Inventory
+  private var inventory: Inventory? = null
+  private var position = RecyclerView.NO_POSITION
+  private lateinit var launcher: ActivityResultLauncher<Intent>
   private lateinit var binding: ActivityInventoryDetailsBinding
   private val locale = Locale("es", "CR")
   private val formatter = DateFormat.getDateInstance(DateFormat.DEFAULT, locale)
+  private lateinit var state: State
 
   @RequiresApi(Build.VERSION_CODES.TIRAMISU)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    state = State.getInstance(this)
     binding = ActivityInventoryDetailsBinding.inflate(layoutInflater)
+    launcher =
+      registerForActivityResult(ActivityResultContracts.StartActivityForResult(), ::resultHandler)
     db = FirebaseFirestore.getInstance()
     setContentView(binding.root)
 
@@ -38,34 +49,68 @@ class InventoryDetailsActivity : AppCompatActivity() {
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
     // TODO: The position will be used to update the inventory in the local sharedInventories array.
-    intent.getIntExtra("position", RecyclerView.NO_POSITION)
-    inventory = intent.getSerializableExtra("inventory") as Inventory
 
-    binding.sharedInventoryName.text = inventory.name
-    binding.sharedInventoryCreationDate.text = formatter.format(Date(inventory.creationDate))
+    loadInventoryData(intent)
 
-    // TODO: Find a different way of doing this, somethig more idiomatic, or somethingdsds
-    // that uses the string resources.
-    binding.sharedInventoryStatus.text = if (inventory.active) {
-      "Activo"
-    } else {
-      "Inactivo"
-    }
+    /*if (state.user.email.compareTo(inventory.ownerEmail) != 0) {
+      binding.floatingActionButtonEditShared.visibility = View.GONE
+    }*/
+  }
 
-    db.document(
-      "/users/${inventory.ownerEmail}"
-    ).get().addOnSuccessListener { doc ->
-      owner = doc.toObject(User::class.java)!!
-      binding.sharedInventoryOwner.text = owner.name
+  private fun loadInventoryData(intent: Intent) {
+    inventory = intent.getSerializableExtra("inventory") as Inventory?
+    position = intent.getIntExtra("position", RecyclerView.NO_POSITION)
+    inventory?.let {
+      binding.sharedInventoryName.text = it.name
+      binding.sharedInventoryCreationDate.text = formatter.format(Date(it.creationDate))
+
+      // TODO: Find a different way of doing this, somethig more idiomatic, or somethingdsds
+      //  that uses the string resources.
+      binding.sharedInventoryStatus.text = if (it.active) {
+        "Activo"
+      } else {
+        "Inactivo"
+      }
+
+      db.document(
+        "/users/${it.ownerEmail}"
+      ).get().addOnSuccessListener { doc ->
+        owner = doc.toObject(User::class.java)!!
+        binding.sharedInventoryOwner.text = owner.name
+      }
     }
   }
 
   fun listInvitedUsers(view: View) {
     // launch the activity to list the users
-    val intent = Intent(this, InvitedUsersListActivity::class.java)
+    val clazz = if (state.user.email.compareTo(inventory!!.ownerEmail) == 0) {
+      // TODO: Aquí se tiene que poner la clase para la actividad
+      TODO("TODO: Aquí se debe de colocar la clase que muestra los invitados.")
+    } else {
+      InvitedUsersListGuestActivity::class.java
+    }
+    val intent = Intent(this, clazz)
+    intent.putExtra("position", position)
     intent.putExtra("inventory", inventory)
-    startActivity(intent)
+    launcher.launch(intent)
+  }
 
+  fun editInventory(view: View) {
+    val intent = Intent(this, EditInventoryActivity::class.java)
+    intent.putExtra("position", position)
+    intent.putExtra("inventory", inventory)
+    launcher.launch(intent)
+  }
+
+  private fun resultHandler(result: ActivityResult) {
+    when (result.resultCode) {
+      EDITED_INVENTORY -> {
+        loadInventoryData(result.data!!)
+        setResult(
+          EDITED_INVENTORY, result.data
+        )
+      }
+    }
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
