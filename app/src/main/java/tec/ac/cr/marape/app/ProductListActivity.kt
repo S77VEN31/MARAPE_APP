2a67ef81a49de2f1141b0cad1224d4717a5fd3c8
Mariana <mfernandezmar3@gmail.com>
2024-01-07 19:41:41 -0600

    feat: Unlink product, open edit product activity and some code to delete product

--- app/src/main/java/tec/ac/cr/marape/app/ProductListActivity.kt
+++ app/src/main/java/tec/ac/cr/marape/app/ProductListActivity.kt
@@ -26,17 +26,36 @@ class ProductListActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
         db = FirebaseFirestore.getInstance()
         inventory = intent.getSerializableExtra("inventory")!! as Inventory
+
+        lifecycleScope.launch {
+          productList = inventory.items.mapNotNull { invitedUser ->
+            if (invitedUser.isNotBlank()) {
+              val productDocument = db.document("products/$invitedUser").get().await()
+              productDocument?.toObject(Product::class.java)
+            } else {
+              null // Si invitedUser es nulo o vacío, devolveremos null
+            }
+          } ?: listOf() // Si inventory es nulo, asignamos una lista vacía
+
+          binding.productList.adapter = ProductListAdapter(productList ?: listOf(), inventory)
+          binding.productList.layoutManager = LinearLayoutManager(this@ProductListActivity)
+      }
+
+
+    }
+
+  /*
+
         lifecycleScope.launch {
           productList = inventory.items.map { invitedUser ->
             db.document("products/${invitedUser}").get().await().toObject(Product::class.java)!!
           }
-          binding.productList.adapter = ProductListAdapter(productList)
+          binding.productList.adapter = ProductListAdapter(productList, inventory)
           binding.productList.layoutManager = LinearLayoutManager(this@ProductListActivity)
         }
-    }
+   */
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
       return when (item.itemId) {
