a816170cefc0727f561297472f66ee5fb6bf211f
zSnails <erizojuan33@gmail.com>
2024-01-09 22:56:39 -0600

    fix: Extract UI logic from the adapter into the activity.

--- app/src/main/java/tec/ac/cr/marape/app/ExportProductsActivity.kt
+++ app/src/main/java/tec/ac/cr/marape/app/ExportProductsActivity.kt
@@ -1,18 +1,26 @@
 package tec.ac.cr.marape.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.SearchView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import tec.ac.cr.marape.app.adapter.EDITED_PRODUCT
 import tec.ac.cr.marape.app.adapter.ExportProductAdapter
 import tec.ac.cr.marape.app.databinding.ActivityExportProductsBinding
 import tec.ac.cr.marape.app.model.Product
@@ -21,7 +29,10 @@ class ExportProductsActivity : AppCompatActivity() {
   private lateinit var db: FirebaseFirestore
   private lateinit var binding: ActivityExportProductsBinding
   private lateinit var productsAdapter: ExportProductAdapter
-  private lateinit var productList: List<Product>
+  private val launcher: ActivityResultLauncher<Intent> = registerForActivityResult(
+    ActivityResultContracts.StartActivityForResult(), ::resultHandler
+  )
+  private lateinit var productList: ArrayList<Product>
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityExportProductsBinding.inflate(layoutInflater)
@@ -31,9 +42,12 @@ class ExportProductsActivity : AppCompatActivity() {
     db = FirebaseFirestore.getInstance()
 
     lifecycleScope.launch {
-      productList = db.collection("products").get().await().toObjects(Product::class.java)
+      productList =
+        db.collection("products").get().await().toObjects(Product::class.java) as ArrayList<Product>
 
-      productsAdapter = ExportProductAdapter(productList, this@ExportProductsActivity)
+      productsAdapter = ExportProductAdapter(productList)
+      productsAdapter.addEditListener(::editListener)
+      productsAdapter.addDeleteListener(::deleteListener)
       binding.exportProductList.adapter = productsAdapter
       binding.exportProductList.layoutManager = LinearLayoutManager(this@ExportProductsActivity)
     }
@@ -56,10 +70,109 @@ class ExportProductsActivity : AppCompatActivity() {
 
         })
       }
+
       override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
         return false
       }
     }, this)
+  }
+
+
+  private fun isProductAssociated(productId: String, onComplete: (Boolean) -> Unit) {
+    // Consulta para verificar si el producto está presente en algún inventario
+    db.collection("inventories").whereArrayContains("items", productId).get()
+      .addOnSuccessListener { inventoryQuerySnapshot ->
+        onComplete(!inventoryQuerySnapshot.isEmpty)
+      }.addOnFailureListener {
+        onComplete(false) // Error al consultar los inventarios
+      }
+  }
+
+  private fun resultHandler(result: ActivityResult) {
+    when (result.resultCode) {
+      EDITED_PRODUCT -> {
+        val position = result.data?.getIntExtra("position", RecyclerView.NO_POSITION)!!
+        val product = result.data?.getSerializableExtra("product")!! as Product
+        productsAdapter.update(position, product)
+      }
+    }
+  }
+
+  private fun sendProductAssociatedDialog() {
+    val builder = AlertDialog.Builder(this)
+    builder.setTitle("Producto asociado")
+    builder.setMessage("Este producto no se puede eliminar porque está asociado a algún inventario.")
+
+    builder.setPositiveButton("Aceptar") { dialog, _ ->
+      dialog.dismiss()
+    }
+
+    val dialog = builder.create()
+    dialog.show()
+  }
+
+  private fun deleteListener(position: Int, product: Product) {
+    isProductAssociated(product.id) { success ->
+      if (success) {
+        sendProductAssociatedDialog()
+      } else {
+        sendConfirmation {
+          showResultDelete(position, product)
+        }
+      }
+    }
+  }
+
+  // ---------------------------------------- EDIT PRODUCT ---------------------------------------------
+  private fun editListener(position: Int, product: Product) {
+    val intent = Intent(this, EditProductActivity::class.java)
+    intent.putExtra("position", position)
+    intent.putExtra("product", product)
+    launcher.launch(intent)
+  }
+
+  private fun deletePerformance(
+    position: Int, currentProduct: Product, onComplete: (Boolean) -> Unit
+  ) {
+    db.collection("products").document(currentProduct.id).delete().addOnSuccessListener {
+      productsAdapter.removeProduct(position, currentProduct)
+      onComplete(true)
+    }.addOnFailureListener {
+      onComplete(false)
+    }
+  }
+
+  private fun showResultDelete(position: Int, product: Product) {
+    deletePerformance(position, product) { success ->
+      if (success) {
+        Toast.makeText(
+          this, "Producto eliminado con éxito", Toast.LENGTH_SHORT
+        ).show()
+      } else {
+        Toast.makeText(
+          this, "Error al eliminar el producto", Toast.LENGTH_SHORT
+        ).show()
+      }
+    }
+  }
+
+  private fun sendConfirmation(
+    onYes: () -> Unit
+  ) {
+    val builder = AlertDialog.Builder(this)
+    builder.setTitle("Confirmación")
+    builder.setMessage("¿Está seguro de eliminar este producto?")
+
+    builder.setPositiveButton("Sí") { dialog, which ->
+      onYes()
+    }
+
+    builder.setNegativeButton("No") { dialog, which ->
+      dialog.dismiss()
+    }
+
+    val dialog = builder.create()
+    dialog.show()
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/tec/ac/cr/marape/app/adapter/ExportProductAdapter.kt
+++ app/src/main/java/tec/ac/cr/marape/app/adapter/ExportProductAdapter.kt
@@ -1,41 +1,40 @@
 package tec.ac.cr.marape.app.adapter
 
 import android.annotation.SuppressLint
-import android.app.AlertDialog
-import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
 import android.widget.ImageButton
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.FieldValue
-import com.google.firebase.firestore.FirebaseFirestore
 import me.xdrop.fuzzywuzzy.FuzzySearch
-import tec.ac.cr.marape.app.EditProductActivity
-import tec.ac.cr.marape.app.ExportProductsActivity
 import tec.ac.cr.marape.app.R
 import tec.ac.cr.marape.app.model.Product
 
 
-class ExportProductAdapter(var productList: List<Product>, private val activity: ExportProductsActivity) :
-  RecyclerView.Adapter<ExportProductAdapter.ViewHolder>(), Filterable {
-  private val db = FirebaseFirestore.getInstance()
-  private lateinit var launcher: ActivityResultLauncher<Intent>
+class ExportProductAdapter(
+  var productList: MutableList<Product>,
+) : RecyclerView.Adapter<ExportProductAdapter.ViewHolder>(), Filterable {
   private var filteredProducts = productList
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-    val itemView =
-      LayoutInflater.from(parent.context).inflate(R.layout.layout_export_product_item, parent, false)
+    val itemView = LayoutInflater.from(parent.context)
+      .inflate(R.layout.layout_export_product_item, parent, false)
     return ViewHolder(itemView)
   }
 
   override fun getItemCount(): Int {
     return filteredProducts.size
+  }
+
+  private lateinit var editListener: (Int, Product) -> Unit
+  fun addEditListener(listener: (Int, Product) -> Unit) {
+    editListener = listener
+  }
+
+  private lateinit var deleteListener: (Int, Product) -> Unit
+  fun addDeleteListener(listener: (Int, Product) -> Unit) {
+    deleteListener = listener
   }
 
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -45,19 +44,12 @@ class ExportProductAdapter(var productList: List<Product>, private val activity:
     holder.productAmount.text = currentProduct.amount.toString()
 
     holder.productDelete.setOnClickListener {
-      isProductAssociated(currentProduct.id) { success ->
-        if (success) {
-          sendProductAssociatedDialog(holder.itemView.context)
-        } else {
-          sendConfirmation(holder.itemView.context) {
-            showResultDelete(holder.itemView.context, currentProduct)
-          }
-        }
-      }
+      deleteListener(position, currentProduct)
     }
 
     holder.productEdit.setOnClickListener {
-      goToEditProduct(holder.itemView.context, currentProduct.id)
+      //goToEditProduct(holder.itemView.context, currentProduct.id)
+      editListener(position, currentProduct)
     }
   }
 
@@ -97,118 +89,33 @@ class ExportProductAdapter(var productList: List<Product>, private val activity:
     return filter
   }
 
-  // ---------------------------------------- EDIT PRODUCT ---------------------------------------------
-
-  init {
-    setupLauncher()
-  }
-
-  private fun setupLauncher() {
-    launcher = activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-      if (result.resultCode == REQUEST_CODE_EDIT_PRODUCT) {
-        val updatedProductId = result.data?.getStringExtra("updatedProductId")
-        updatedProductId?.let { productId ->
-          // Obtener el producto actualizado de la base de datos con el ID
-          db.document("products/$productId").get().addOnSuccessListener { documentSnapshot ->
-            val updatedProduct = documentSnapshot.toObject(Product::class.java)
-            updatedProduct?.let { product ->
-              // Actualizar el producto en la lista productList
-              val index = productList.indexOfFirst { it.id == productId }
-              if (index != -1) {
-                productList = productList.toMutableList().apply {
-                  set(index, product)
-                }
-                notifyDataSetChanged()
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-  private fun goToEditProduct(context: Context, productId: String) {
-    val intent = Intent(context, EditProductActivity::class.java)
-    intent.putExtra("product_id", productId)
-    launcher.launch(intent)
-  }
 
   // ----------------------------------------- DELETE PRODUCT -------------------------------------------------
+  fun removeProduct(position: Int, product: Product) {
+    val idx = productList.indexOfFirst {
+      it.id == product.id
+    }
+    if (idx != -1) {
+      productList.removeAt(idx)
+    }
+    if (filteredProducts != productList) {
+      filteredProducts.removeAt(position)
+    }
+    notifyItemRemoved(position)
+  }
 
-  private fun sendConfirmation(
-    context: Context, onYes: () -> Unit
-  ) {
-    val builder = AlertDialog.Builder(context)
-    builder.setTitle("Confirmación")
-    builder.setMessage("¿Está seguro de eliminar este producto?")
-
-    builder.setPositiveButton("Sí") { dialog, which ->
-      onYes()
+  fun update(position: Int, product: Product) {
+    val idx = productList.indexOfFirst {
+      it.id == product.id
+    }
+    if (idx != -1) {
+      productList[idx] = product
     }
 
-    builder.setNegativeButton("No") { dialog, which ->
-      dialog.dismiss()
+    if (productList != filteredProducts) {
+      filteredProducts[position] = product
     }
-
-    val dialog = builder.create()
-    dialog.show()
+    notifyItemChanged(position)
   }
-
-  private fun sendProductAssociatedDialog(context: Context) {
-    val builder = AlertDialog.Builder(context)
-    builder.setTitle("Producto asociado")
-    builder.setMessage("Este producto no se puede eliminar porque está asociado a algún inventario.")
-
-    builder.setPositiveButton("Aceptar") { dialog, _ ->
-      dialog.dismiss()
-    }
-
-    val dialog = builder.create()
-    dialog.show()
-  }
-
-
-  private fun isProductAssociated(productId: String, onComplete: (Boolean) -> Unit) {
-    // Consulta para verificar si el producto está presente en algún inventario
-    db.collection("inventories")
-      .whereArrayContains("items", productId)
-      .get()
-      .addOnSuccessListener { inventoryQuerySnapshot ->
-        onComplete(!inventoryQuerySnapshot.isEmpty)
-      }.addOnFailureListener {
-        onComplete(false) // Error al consultar los inventarios
-      }
-  }
-
-  private fun removeProduct(removeProduct: Product) {
-    val mutableProductList = productList.toMutableList()
-    mutableProductList.remove(removeProduct)
-    productList = mutableProductList.toList()
-  }
-
-  private fun deletePerformance(currentProduct: Product, onComplete: (Boolean) -> Unit) {
-    db.collection("products").document(currentProduct.id).delete().addOnSuccessListener {
-      removeProduct(currentProduct)
-      onComplete(true)
-    }.addOnFailureListener {
-      onComplete(false)
-    }
-  }
-
-  private fun showResultDelete(context: Context, currentProduct: Product) {
-    deletePerformance(currentProduct) { success ->
-      if (success) {
-        Toast.makeText(
-          context, "Producto eliminado con éxito", Toast.LENGTH_SHORT
-        ).show()
-        notifyDataSetChanged()
-      } else {
-        Toast.makeText(
-          context, "Error al eliminar el producto", Toast.LENGTH_SHORT
-        ).show()
-      }
-    }
-  }
-
 }
 
